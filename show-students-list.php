cc14225c326fe984934cf9d4df090cc8867e711d
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 00:12:41 +0100

    Ajouter icon details

--- show-students-list.php
+++ show-students-list.php
@@ -19,7 +19,7 @@ $students=$student->showList();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste Etudiant</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"> 
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
 
@@ -31,7 +31,8 @@ $students=$student->showList();
         <tr>
             <th>id</th>
             <th>name</th>
-            <th>birthday</th>        
+            <th>birthday</th>  
+            <th></th>      
     
         </tr>
         </thead>
@@ -48,7 +49,8 @@ $students=$student->showList();
          <tr>
             <td><?= $student->id?></td>
             <td><?= $student->name?></td>
-            <td><?= $student->birthday?></td>            
+            <td><?= $student->birthday?></td> 
+            <td><a href="show-student-details.php?id=<?= $student->id; ?>"><i class="fa-solid fa-circle-info "></i></a>           
             
         </tr>
        <?php } ?>
