3e500b43ef86d9858a89e0d51552efacd9fa5a2a
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 01:12:55 +0100

    Remplir table section

---
+++ remplir-table-section.php
@@ -0,0 +1,15 @@
+<?php
+require 'autoload.php';
+$section_1=new section(1,'GL','genie logiciel');
+$section_2=new section(2,'RT','reseaux et telecom');
+$section_3=new section(3,'IIA','informatique industrielle');
+$section_4=new section(4,'IMI','maintenance');
+$section_5=new section(5,'CH','chimie');
+$section_6=new section(6,'BIO','biologie');
+
+$section_1->addSection();
+$section_2->addSection();
+$section_3->addSection();
+$section_4->addSection();
+$section_5->addSection();
+$section_6->addSection();
