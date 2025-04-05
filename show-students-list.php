6d64e27c53055579608039722f8001157976416c
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 02:27:50 +0100

    Ajouter image

--- show-students-list.php
+++ show-students-list.php
@@ -31,9 +31,11 @@ session_start();
         <thead>
         <tr>
             <th>id</th>
+            <th>Images</th> 
             <th>name</th>
             <th>birthday</th>  
             <th>Section</th>  
+             
             <th></th> 
                
     
@@ -51,13 +53,15 @@ session_start();
 
          <tr>
             <td><?= $student->id?></td>
+            <td><img src=<?= $student->imgpath ?> style="width: 50px; height: 50px;"></td> 
             <td><?= $student->name?></td>
             <td><?= $student->birthday?></td> 
             <td><?= $student->section?></td>
+            
             <td><a href="show-student-details.php?id=<?= $student->id; ?>"><i class="fa-solid fa-circle-info "></i></a>           
             <?php if($_SESSION['user']['role']=='admin'){ ?>
             <a href="delete-student.php?id=<?= $student->id; ?>"><i class="fa-solid fa-eraser"></i></a>
-            <a href="modification.php?id=<?= $student->id; ?>"><i class="fa-solid fa-pen-to-square"></i></a> <?php }?>   </td> 
+            <a href="new-student-credentials.php?id=<?= $student->id; ?>"><i class="fa-solid fa-pen-to-square"></i></a> <?php }?>   </td> 
         </tr>
        <?php } ?>
          </tbody>
