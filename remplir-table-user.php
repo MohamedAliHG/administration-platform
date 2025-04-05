825fd6ebc8b86ff6991bc1131ca69935857c01e5
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 13:27:35 +0100

    remove unecessaty files

--- remplir-table-user.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'autoload.php';
-$user_1=new user(12315,'med','med@gmail.com','admin','1234');
-$user_2=new user(45612,'ali','ali@gmail.com','user','azerty');
-
-$user_1->addUser();
-$user_2->addUser();
