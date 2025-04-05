53e40c3a4de5e16a91d818dc5ab54551b8419eb6
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 02:22:47 +0100

    recevoir les donnees

---
+++ modify-student.php
@@ -0,0 +1,22 @@
+<?php 
+
+require 'autoload.php';
+
+$id=$_POST['id'];
+$newid=$_POST['newid'];
+$name=$_POST['name'];
+$birthday=$_POST['date'];
+$path=__DIR__.'\\'.$_FILES['fichier']['name'];
+
+if(move_uploaded_file($_FILES['fichier']['tmp_name'],$path)){
+        $imgpath=$_FILES['fichier']['name'];    
+   
+    }    
+else{
+        echo 'failed to upload';
+    }
+
+
+$modification=new student();
+$modification->modifyStudent($id,$newid,$name,$birthday,$imgpath);
+header('Location: show-students-list.php');
