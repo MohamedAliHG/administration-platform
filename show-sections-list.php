6ac20b38063c912b435e14190965425fc1b333bf
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 02:40:06 +0100

    Ajouter fontAwesome

--- show-sections-list.php
+++ show-sections-list.php
@@ -17,7 +17,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des sections</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css"> 
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
 </head>
 <body>
