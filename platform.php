f1dc3a49645dbe56efae9e5b588a127c3abbf20b
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 01:30:37 +0100

    platform interface

---
+++ platform.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <header>
+    <?php include 'navbar.php'  ; ?>
+    </header>
+    <main>
+        <h1>Welcom to the administration Platform</h1>
+    </main>
+</body>
+</html>
