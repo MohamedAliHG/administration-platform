0c01a77533b9a2c94f61e5d47a414b8a7604df2e
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-04-05 01:32:19 +0100

    Ajouter navbar

--- show-students-list.php
+++ show-students-list.php
@@ -22,7 +22,7 @@ $students=$student->showList();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
-
+<?php include 'navbar.php'  ; ?>
   <br>
     <div class="container">
 
